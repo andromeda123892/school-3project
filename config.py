fa197de368d7bdeeda8cffd6c2b5caf1623f164a
Andrey <abuldigirov@inbox.ru>
2022-02-04 21:59:41 +0300

    Добавили базу данных

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #Файл конфигурации
-host = "127.0.0.1"
-user = "postgres"
-password = "123123"
-db_name = "library"
+host = "ec2-54-229-47-120.eu-west-1.compute.amazonaws.com"
+user = "ccolojyndftvni"
+password = "d7bad67a0128c7455996c0b14235480bf58dfdcf51c1e1ecdc1855990a99f071"
+db_name = "db3aq4rmlu3jqk"
 port = 5432
