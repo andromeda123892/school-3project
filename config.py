b7ddea957449158ca834df589d534eafdc61e462
Andrey <abuldigirov@inbox.ru>
2022-03-07 00:11:30 +0300

    d

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #Файл конфигурации
-host = "ec2-54-229-47-120.eu-west-1.compute.amazonaws.com"
-user = "ccolojyndftvni"
-password = "d7bad67a0128c7455996c0b14235480bf58dfdcf51c1e1ecdc1855990a99f071"
-db_name = "db3aq4rmlu3jqk"
+host = "ec2-52-215-225-178.eu-west-1.compute.amazonaws.com"
+user = "qbbiamxylplgsy"
+password = "0509a393a87233611dcd68420957cd52b0a0453df55eb0c1c1a03ee331d665fc"
+db_name = "dbl89n9q8mfp0g"
 port = 5432
